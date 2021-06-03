bec0ca0be0193d69b0eacf79936d9c54e99be8c9
Orress35 <79585248+Orress35@users.noreply.github.com>
2021-06-03 10:18:27 +0300

    Create Form1.vb

---
+++ Form1.vb
@@ -0,0 +1,29 @@
+Public Class Form1
+    Dim x, y, z As Integer
+
+    Private Sub CalcButton_Click(sender As Object, e As EventArgs) Handles CalcButton.Click
+        If EquationBox.Text.Contains("+") Then
+            x = Integer.Parse(EquationBox.Text.Split("+")(0))
+            y = Integer.Parse(EquationBox.Text.Split("+")(1))
+            z = x + y
+            ResultLabel.Text = "Result: " + z.ToString()
+        ElseIf EquationBox.Text.Contains("-") Then
+            x = Integer.Parse(EquationBox.Text.Split("-")(0))
+            y = Integer.Parse(EquationBox.Text.Split("-")(1))
+            z = x - y
+            ResultLabel.Text = "Result: " + z.ToString()
+        ElseIf EquationBox.Text.Contains("*") Then
+            x = Integer.Parse(EquationBox.Text.Split("*")(0))
+            y = Integer.Parse(EquationBox.Text.Split("*")(1))
+            z = x * y
+            ResultLabel.Text = "Result: " + z.ToString()
+        ElseIf EquationBox.Text.Contains("/") Then
+            x = Integer.Parse(EquationBox.Text.Split("/")(0))
+            y = Integer.Parse(EquationBox.Text.Split("/")(1))
+            z = x / y
+            ResultLabel.Text = "Result: " + z.ToString()
+        Else
+            ResultLabel.Text = "Result: No operator found"
+        End If
+    End Sub
+End Class
